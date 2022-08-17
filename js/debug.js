dc3ead46608b8acc62cadb0146d3d4d61c23e714
Moodybleu <meganbenn27@icloud.com>
2022-08-17 09:52:10 -0700

    Finishes debugging

--- js/debug.js
+++ js/debug.js
@@ -1,10 +1,11 @@
-console.log("Hello Debuggers!")
 
-document.addEventListener('DOMcontentLoaded', function(){
+
+document.addEventListener('DOMContentLoaded', function(){
+  console.log("Hello Debuggers!")
   console.log("Let's learn how to debug.");
 
   var x = 2;
-  let result = Math.power(x, x); //expects 2^2 equals 4
+  let result = Math.pow(x, x); //expects 2^2 equals 4
   console.log("Got", result, "Expected 4");
 
   let y = 4;
@@ -12,12 +13,12 @@ document.addEventListener('DOMcontentLoaded', function(){
   console.log("Got", result2, "Expected 6");
 
   let z = { width: 5, height: 7};
-  console.log("z is ", z.getWidth(), "inches wide."); //expect "z is 5 inches wide"
+  console.log("z is ", z.width, "inches wide."); //expect "z is 5 inches wide"
 
   let q = 100;
-  if(q > 50);{
+  if(q > 50){
     console.log("q is a big number"); //this is what should print to the console
-  } else {
+  } else{
     console.log("q is less than 50 dawg")
   };
 
@@ -25,12 +26,12 @@ document.addEventListener('DOMcontentLoaded', function(){
   console.log(amount + 13); //should print out 26
 
   const num = "37";
-  if(num = "5"){
+  if(num == "5"){
     console.log("My num is", num); //This should not get printed since num is "37"
   }
 
   var jason = "dope dude";
-  if(typeof jackson === "string"){
+  if(typeof jason === "string"){
     console.log("jackson is a", jason);//should print "jackson is a dope dude", which is true
   }
 
